9f10eed2c76940630686724754033deca53bce6b
realquizm <emil@westenius.se>
2015-04-10 16:15:31 +0200

    nestan allt

--- clientserver/filesystemdatabase.d
+++
@@ -1,2 +0,0 @@
-filesystemdatabase.o filesystemdatabase.d : filesystemdatabase.cc filesystemdatabase.h \
-  newsgroup.h utility.h article.h database.h
--- clientserver/inmemorydatabase.d
+++
@@ -1,2 +0,0 @@
-inmemorydatabase.o inmemorydatabase.d : inmemorydatabase.cc inmemorydatabase.h newsgroup.h \
-  utility.h article.h database.h
